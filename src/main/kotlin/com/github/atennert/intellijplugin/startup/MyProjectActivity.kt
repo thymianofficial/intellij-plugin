a3997f4ad2c21a597b6aa7086166ac085a25222a
GitHub Action <action@github.com>
2025-09-25 11:52:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/atennert/intellijplugin/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/atennert/intellijplugin/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.atennert.intellijplugin.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
