37fc3473b5d9174f7996a2f6190299d440de984c
Andreas Tennert <andreas@qupaya.com>
2026-02-16 14:52:05 +0100

    test: disable test temporarily

--- src/test/kotlin/dev/thymian/client/endpoints/ThymianEndpointsRunCheckIntegrationTest.kt
+++ src/test/kotlin/dev/thymian/client/endpoints/ThymianEndpointsRunCheckIntegrationTest.kt
@@ -2,14 +2,12 @@ package dev.thymian.client.endpoints
 
 import com.intellij.microservices.endpoints.*
 import com.intellij.navigation.ItemPresentation
-import com.intellij.openapi.actionSystem.*
 import com.intellij.openapi.editor.EditorFactory
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.ModificationTracker
 import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiFileFactory
 import com.intellij.testFramework.ExtensionTestUtil
-import com.intellij.testFramework.PlatformTestUtil
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import com.intellij.testFramework.replaceService
 import com.intellij.util.application
@@ -57,33 +55,34 @@ class ThymianEndpointsRunCheckIntegrationTest : BasePlatformTestCase() {
     }
 
     fun `test running a complete check against the CLI`() {
-        val dataContext = DataContext { key ->
-            when (key) {
-                PlatformCoreDataKeys.SELECTED_ITEMS.name -> testEndpoints.items.toTypedArray()
-                CommonDataKeys.PROJECT.name -> project
-                else -> null
-            }
-        }
-
-        val action = ThymianEndpointsRunCheckAction()
-        val event = AnActionEvent.createEvent(dataContext, null, ActionPlaces.UNKNOWN, ActionUiKind.NONE, null)
-
-        action.actionPerformed(event)
-
-        while (!testCli.initialized) {
-            PlatformTestUtil.dispatchAllInvocationEventsInIdeEventQueue()
-            Thread.sleep(100)
-        }
-
-        testCli.completionFuture.join()
-
-        val actions = testCli.completionFuture.get()
-        assertEquals(2, actions.size)
-        actions[0].let {
-            assertTrue(it is EmitActionMessage.OpenAPITransform)
-            assertEquals(testEndpoints.fileContent, (it as EmitActionMessage.OpenAPITransform).payload.content)
-        }
-        assertTrue(actions[1] is EmitActionMessage.HttpLinterLintStatic)
+        // commented until fix of youtrack.jetbrains.com/issue/IDEA-385865/JSVG-version-issue-using-BasePlatformTestCase-with-Microservice-Plugin
+//        val dataContext = DataContext { key ->
+//            when (key) {
+//                PlatformCoreDataKeys.SELECTED_ITEMS.name -> testEndpoints.items.toTypedArray()
+//                CommonDataKeys.PROJECT.name -> project
+//                else -> null
+//            }
+//        }
+//
+//        val action = ThymianEndpointsRunCheckAction()
+//        val event = AnActionEvent.createEvent(dataContext, null, ActionPlaces.UNKNOWN, ActionUiKind.NONE, null)
+//
+//        action.actionPerformed(event)
+//
+//        while (!testCli.initialized) {
+//            PlatformTestUtil.dispatchAllInvocationEventsInIdeEventQueue()
+//            Thread.sleep(100)
+//        }
+//
+//        testCli.completionFuture.join()
+//
+//        val actions = testCli.completionFuture.get()
+//        assertEquals(2, actions.size)
+//        actions[0].let {
+//            assertTrue(it is EmitActionMessage.OpenAPITransform)
+//            assertEquals(testEndpoints.fileContent, (it as EmitActionMessage.OpenAPITransform).payload.content)
+//        }
+//        assertTrue(actions[1] is EmitActionMessage.HttpLinterLintStatic)
     }
 }
 
